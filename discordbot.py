6a737a97303a3d6677ac03d7de0e4d6d6507b2fa
ChenChenIan <45452717+ChenChenIan@users.noreply.github.com>
2021-12-16 17:41:40 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,25 +14,24 @@ client = discord.Client()
 async def on_ready():
     print('成功登入')
 
-# 收到訊息時呼叫
 @client.event
+#當有訊息時
 async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
+    #排除自己的訊息，避免陷入無限循環
+    print(message.content)
+    if message.author == client.user:
         return
-    
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
+    #如果以「say」開頭
+    if message.content.startswith("say"):
+      #將訊息切一刀，也就是變成兩份
+      tmp = message.content.split(" ",1)
+      #如果分割後串列長度只有1，代表沒有輸入後面要說的內容
+      print(tmp)
+      if len(tmp) == 1:
+        await message.channel.send(f"{message.author.mention} 你要我說什麼啦？")
+
+      else:
+        await message.channel.send(f"{message.author.mention} 他逼得我說「{tmp[1]}」")
 
 # Bot起動
 client.run(TOKEN)
